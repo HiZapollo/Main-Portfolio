f194c2cc4f8dc3490f38789fc7ab40fa99dafd36
zeckenrode <zeckenrode9000@gmail.com>
2023-11-19 17:56:53 -0500

    Removed useState from navbar, as it was not needed

--- portfolio/src/components/Navbar/index.jsx
+++ portfolio/src/components/Navbar/index.jsx
@@ -1,18 +1,7 @@
-import { useState } from 'react';
 
 import './style.css'
 
 const Nav = () => {
-
-    const [hamburgerActive, setHamburgerActive] = useState('inactive')
-
-    const handleHamburger = () => {
-        if (hamburgerActive === 'active') {
-            setHamburgerActive('inactive')
-        } else {
-            setHamburgerActive('active')
-        }
-    }
 
     return (
         <>
@@ -20,18 +9,19 @@ const Nav = () => {
                 <h1 className='m-2'>
                     Zeckenrode
                 </h1>
-                <button className={`m-2 md:hidden burgerstack ${hamburgerActive}`} onClick={handleHamburger}>
+                <input className="side-menu hidden" type="checkbox" id="side-menu"/>
+                <label htmlFor='side-menu' className={`m-2 md:hidden burgerstack`}>
                     <div className="burger burger1"></div>
                     <div className="burger burger2"></div>
                     <div className="burger burger3"></div>
-                </button>
-                <div className={`navbar hidden md:block ${hamburgerActive}`}>
-                    <ul className='navlist'>
-                        <li>home</li>
-                        <li>skills</li>
-                        <li>projects</li>
-                        <li>experience</li>
-                        <li>contact</li>
+                </label>
+                <div className={`navbar hidden md:block`}>
+                    <ul className={`navlist`}>
+                        <li><a href="#">home</a></li>
+                        <li><a href="#">skills</a></li>
+                        <li><a href="#">projects</a></li>
+                        <li><a href="#">experience</a></li>
+                        <li><a href="#">contact</a></li>
                     </ul>
                 </div>
             </header>
